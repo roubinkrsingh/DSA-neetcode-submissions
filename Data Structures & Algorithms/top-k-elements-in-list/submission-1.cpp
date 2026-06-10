046f1ba2ee599acf9491af34e298b60c5d7c80a8
roubin kumar singh <66862712+roubinkrsingh@users.noreply.github.com>
2026-06-11 00:26:35 +0530

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        vector<pair<int,int>>v;
+        vector<int>ans;
+        for(auto it:mp){
+            v.push_back({it.second,it.first});
+        }
+        sort(v.begin(),v.end(),greater<pair<int,int>>());
+        for(int i=0;i<k;i++) ans.push_back(v[i].second);
+        return ans;
+        
+    }
+};
