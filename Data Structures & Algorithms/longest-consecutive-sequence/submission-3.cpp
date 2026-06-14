48303a25a94cf423aed42101d65736c9ea7699d7
roubin kumar singh <66862712+roubinkrsingh@users.noreply.github.com>
2026-06-14 17:17:36 +0530

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++) mp[nums[i]]++;
+        int ans=0;
+        vector<int>start_index;
+        for(int i=0;i<n;i++){
+            if(mp.count(nums[i]-1)) continue;
+            start_index.push_back(nums[i]);
+        }
+        for(int i=0;i<start_index.size();i++){
+            int x=start_index[i];
+            int curr_ans=0;
+            while(mp.count(x)){
+                curr_ans++;
+                mp[x]--;
+                if(mp[x]==0) mp.erase(x);
+                x++;
+            }
+            ans=max(ans,curr_ans);
+        }
+        return ans;
+    }
+};
